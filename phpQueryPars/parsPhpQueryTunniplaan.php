c152367cc17e70b5bc9605c00e56096ff77a6229
Erik Libene <33956094+ErikLib2910@users.noreply.github.com>
2017-12-02 04:27:43 -0800

    Create parsPhpQueryTunniplaan.php

---
+++ phpQueryPars/parsPhpQueryTunniplaan.php
@@ -0,0 +1,12 @@
+<?php
+ $url = 'https://tpt.siseveeb.ee/veebivormid/tunniplaan/tunniplaan?oppegrupp=200&nadal=27.11.2017'; 
+ $file = file_get_contents($url);
+ $pattern = '#<div class="btn-group.+?</div>#s';
+ preg_match($pattern, $file, $matches);
+ print_r($matches);
+ ?>
+ 
+ <head>
+  <meta http-equiv="refresh" content="10000">
+ </head>
+ 
